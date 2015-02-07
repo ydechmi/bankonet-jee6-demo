5500b6594f23a847216cd85d93f16c1fa398c030
ydechmi <yacine.dechmi@gmail.com>
2015-02-08 00:29:33 +0100

    Formatting code

--- bankonet-jee6-demo-domaine/src/main/java/com/yd/bankonet/domaine/AbstractEntity.java
+++ bankonet-jee6-demo-domaine/src/main/java/com/yd/bankonet/domaine/AbstractEntity.java
@@ -24,7 +24,7 @@ public abstract class AbstractEntity<ID extends Serializable> implements Seriali
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	@NotNull	
+    @NotNull
 	private ID id;
 
 	/**
