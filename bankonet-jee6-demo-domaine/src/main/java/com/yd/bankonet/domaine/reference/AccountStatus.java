7fe7f78e7c75052982d63ba3e2a965869bbcf31b
ydechmi <yacine.dechmi@gmail.com>
2015-02-22 09:31:39 +0100

    Add named query: AccountStatus.findByCode

--- bankonet-jee6-demo-domaine/src/main/java/com/yd/bankonet/domaine/reference/AccountStatus.java
+++ bankonet-jee6-demo-domaine/src/main/java/com/yd/bankonet/domaine/reference/AccountStatus.java
@@ -13,6 +13,10 @@ import javax.persistence.*;
  */
 @Entity
 @Table(name = "ACCOUNT_STATE")
+@NamedQueries({
+        @NamedQuery(name="AccountStatus.findByCode",
+                query="SELECT a FROM AccountStatus a where a.code=:code")
+})
 @AttributeOverrides({
         @AttributeOverride(name = "id", column = @Column(name = "AST_ID")),
         @AttributeOverride(name = "code", column = @Column(name = "AST_CODE", unique = true)),
