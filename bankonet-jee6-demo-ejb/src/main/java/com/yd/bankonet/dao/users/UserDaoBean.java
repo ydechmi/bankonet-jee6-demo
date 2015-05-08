04ccb46f3b5c7f94926e4d73cc9dc928e19ecf3c
ydechmi <yacine.dechmi@gmail.com>
2015-05-08 11:24:28 +0200

    Implement findByLogin() method

--- bankonet-jee6-demo-ejb/src/main/java/com/yd/bankonet/dao/users/UserDaoBean.java
+++ bankonet-jee6-demo-ejb/src/main/java/com/yd/bankonet/dao/users/UserDaoBean.java
@@ -4,6 +4,7 @@ import com.yd.bankonet.dao.AbstractDao;
 import com.yd.bankonet.domaine.users.User;
 
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * Session Bean implementation class UserDaoBean
@@ -24,5 +25,21 @@ public class UserDaoBean extends AbstractDao<User,Long> {
 	@Override
 	public Class<User> getEntityClass() {
 		return User.class;
+	}
+
+	/**
+	 * Get user by login
+	 * @param login identifier of user
+	 * @return User
+	 */
+	public User findByLogin(String login){
+		List<User> users=getEntityManager().createNamedQuery("findByLogin")
+				.setParameter("login",login).getResultList();
+		if(!users.isEmpty()){
+			return users.get(0);
+		}else{
+			return null;
+		}
+
 	}
 }
