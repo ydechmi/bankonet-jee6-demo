d7f068d871d756bb68a985206358f668af6d213e
ydechmi <yacine.dechmi@gmail.com>
2015-05-08 11:25:31 +0200

    Implement findByCode() method

--- bankonet-jee6-demo-ejb/src/main/java/com/yd/bankonet/dao/reference/RoleDaoBean.java
+++ bankonet-jee6-demo-ejb/src/main/java/com/yd/bankonet/dao/reference/RoleDaoBean.java
@@ -4,6 +4,7 @@ import com.yd.bankonet.dao.AbstractDao;
 import com.yd.bankonet.domaine.reference.Role;
 
 import javax.ejb.Stateless;
+import java.util.List;
 
 /**
  * Created by ydechmi on 11/01/2015.
@@ -20,5 +21,21 @@ public class RoleDaoBean extends AbstractDao<Role,Integer>{
      */
     public Class<Role> getEntityClass() {
         return Role.class;
+    }
+
+    /**
+     * Get role by code
+     * @param code identifier of role
+     * @return Role
+     */
+    public Role findByCode(String code){
+        List<Role> roles=getEntityManager().createNamedQuery("Role.findByCode")
+                .setParameter("code",code).getResultList();
+        if(!roles.isEmpty()){
+            return roles.get(0);
+        }else{
+            return null;
+        }
+
     }
 }
